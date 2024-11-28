80f8660f5909c9fe5f07d754713f1c66fcffb91a
nasima <aitoutalebnasima@gmail.com>
2024-11-28 22:21:59 +0100

    the images move when  the picture is pressed

---
+++ D.NA.js
@@ -0,0 +1,17 @@
+
+
+    function phones(phone) {
+        document.querySelector('.img').src = phone;
+    }
+    let container = 
+
+   
+
+   
+    
+
+
+
+    function color(colo) {
+        document.querySelector('.container').style.background = colo;
+    }
